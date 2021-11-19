99fce5d08e1bbd99141485be405e37ff481448f1
Sergio Victor <sergiovmml@gmail.com>
2021-11-19 20:55:09 -0300

    Teste

---
+++ src/aulaArrays/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Arrays() {
+    return (
+        <View>
+            <Text>oi</Text>
+        </View>
+    )
+}
